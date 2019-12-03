06230b4a6519dad47fd6a062af2167a6f522974f
0624031 <45202122+0624031@users.noreply.github.com>
2019-12-03 15:51:45 +0800

    Add files via upload

---
+++ j1203.java
@@ -0,0 +1,13 @@
+
+class Student {
+    Course[] courses = new Course[10];
+}
+class Course {
+        
+}
+class j1203{
+    public void public static void main(String[] args) {
+        
+    } 
+
+}
